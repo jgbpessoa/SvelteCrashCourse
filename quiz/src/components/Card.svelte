69fefbd7de27df7556a61ee26143fa6067fa08fb
jgbpessoa <jgbpessoa@gmail.com>
2023-07-08 20:18:24 -0300

    feat(quiz): create Card component

---
+++ quiz/src/components/Card.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  interface Quiz {
+    id: number;
+    name: string;
+    imageUrl: string;
+    questions: {
+      id: number;
+      question: string;
+      options: { id: number; label: string; text: string }[];
+      answer: string;
+    }[];
+  }
+
+  export let quiz: Quiz;
+</script>
+
+<div
+  class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer m-2"
+>
+  <img class="h-44" src={quiz.imageUrl} alt={quiz.name} />
+  <div class="flex items-center justify-between py-3 px-2">
+    <h1 class="font-bold">{quiz.name}</h1>
+    <p>{quiz.questions.length} questions</p>
+  </div>
+</div>
--- quiz/src/routes/+page.svelte
+++ quiz/src/routes/+page.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
   import quizzes from "../data.json";
+  import Card from "../components/Card.svelte";
 </script>
 
 <div
   class="quizzes-container w-[700px] m-auto flex justify-center h-screen items-center"
 >
   {#each quizzes as quiz (quiz.id)}
-    <div
-      class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer m-2"
-    >
-      <img class="h-44" src={quiz.imageUrl} alt={quiz.name} />
-      <div class="flex items-center justify-between py-3 px-2">
-        <h1 class="font-bold">{quiz.name}</h1>
-        <p>{quiz.questions.length} questions</p>
-      </div>
-    </div>
+    <Card {quiz} />
   {/each}
 </div>
