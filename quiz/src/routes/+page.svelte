aa21c986654342899cfe7cac0b31788c1d7d6702
jgbpessoa <jgbpessoa@gmail.com>
2023-07-08 20:00:08 -0300

    feat(quiz): add project structure

---
+++ quiz/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import quizzes from "../data.json";
+</script>
+
+<div
+  class="quizzes-container w-[700px] m-auto flex justify-center h-screen items-center"
+>
+  {#each quizzes as quiz (quiz.id)}
+    <div
+      class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer m-2"
+    >
+      <img class="h-44" src={quiz.imageUrl} alt={quiz.name} />
+      <div class="flex items-center justify-between py-3 px-2">
+        <h1 class="font-bold">{quiz.name}</h1>
+        <p>{quiz.questions.length} questions</p>
+      </div>
+    </div>
+  {/each}
+</div>
