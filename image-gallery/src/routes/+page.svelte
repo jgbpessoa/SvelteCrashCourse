7e5fd60526eef8657776cfd9a336d25fc49f4360
jgbpessoa <jgbpessoa@gmail.com>
2023-07-08 17:10:02 -0300

    feat(image-gallery): fetch data from unsplash

--- image-gallery/src/routes/+page.svelte
+++ image-gallery/src/routes/+page.svelte
@@ -1,16 +1,75 @@
+<script lang="ts">
+  import axios from "axios";
+  import { onMount } from "svelte";
+
+  let term = "";
+  let photos: {
+    id: string;
+    alt_description: string;
+    urls: {
+      regular: string;
+    };
+  }[] = [];
+
+  //   const fetchData = async () => {
+  //     const response = await axios.get(
+  //       `https://api.unsplash.com/search/photos?page=1&query=${
+  //         term || "dogs"
+  //       }&client_id=ohUgHGXaeVrUtxi9iAKmmvq6A4YwUfQ9pZJJxR09an4`
+  //     );
+
+  //     photos = response.data.results;
+  //   };
+
+  // Practice: use fetch API and then() instead of axios with async/await
+  const fetchData = () => {
+    const fetchPromise = fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${
+        term || "dogs"
+      }&client_id=ohUgHGXaeVrUtxi9iAKmmvq6A4YwUfQ9pZJJxR09an4`
+    );
+
+    fetchPromise
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Request failed");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        photos = data.results;
+      })
+      .catch((error) => console.error(error));
+  };
+
+  onMount(() => {
+    fetchData();
+  });
+
+  const handleSearch = async () => {
+    if (!term) return;
+    await fetchData();
+    term = "";
+  };
+</script>
+
 <div class="container">
   <div class="header">
     <h1>Image Gallery</h1>
     <div class="input-container">
-      <input type="text" class="input" />
-      <button class="button">Search</button>
+      <input type="text" class="input" bind:value={term} />
+      <button
+        class="button"
+        on:click={() => {
+          handleSearch(term);
+        }}>Search</button
+      >
     </div>
   </div>
   <div class="photos">
-    <img src="" alt="" class="image" />
-    <img src="" alt="" class="image" />
-    <img src="" alt="" class="image" />
-    <img src="" alt="" class="image" />
+    {#each photos as photo (photo.id)}
+      <img class="image" src={photo.urls.regular} alt={photo.alt_description} />
+    {/each}
   </div>
 </div>
 
