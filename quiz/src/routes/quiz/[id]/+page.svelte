afaf231fbaf3d2757749695bd0ed8596dacf1128
jgbpessoa <jgbpessoa@gmail.com>
2023-07-09 01:44:59 -0300

    feat(quiz): update global store

--- quiz/src/routes/quiz/[id]/+page.svelte
+++ quiz/src/routes/quiz/[id]/+page.svelte
@@ -3,11 +3,49 @@
   import QuestionOption from "./components/QuestionOption.svelte";
   import QuestionText from "./components/QuestionText.svelte";
   import { answers, type Answer } from "../../../store";
+  import type { SvelteComponent_1 } from "svelte";
+  import { goto } from "$app/navigation";
 
   export let data: any;
 
   let currentQuestionIndex = 0;
   let answersValue: Answer[];
+  let selectedOption: null | string = null;
+  let showCorrectAnswer = false;
+
+  const handleChangeOption = (label: string) => {
+    selectedOption = label;
+  };
+
+  const handleSubmit = () => {
+    if (!selectedOption) return;
+
+    showCorrectAnswer = true;
+
+    answers.update((currentState) => {
+      const updatedAnswerState = currentState;
+
+      const object = {
+        id: currentQuestionIndex + 1,
+        isCorrect: selectedOption === question.answer,
+      };
+      updatedAnswerState.push(object);
+
+      return updatedAnswerState;
+    });
+  };
+
+  const handleNext = () => {
+    showCorrectAnswer = false;
+    selectedOption = null;
+
+    if (currentQuestionIndex === data.questions.length - 1) {
+      goto("/result");
+      return;
+    }
+
+    currentQuestionIndex++;
+  };
 
   answers.subscribe((value) => {
     answersValue = value;
@@ -20,8 +58,14 @@
   <QuestionText text={question.question} />
   <div class="flex justify-between flex-wrap">
     {#each question.options as option (option.id)}
-      <QuestionOption {option} />
+      <QuestionOption
+        {option}
+        {selectedOption}
+        {handleChangeOption}
+        {showCorrectAnswer}
+        answer={question.answer}
+      />
     {/each}
   </div>
-  <QuestionButton />
+  <QuestionButton {handleSubmit} {handleNext} {showCorrectAnswer} />
 </div>
--- quiz/src/routes/quiz/[id]/components/QuestionButton.svelte
+++ quiz/src/routes/quiz/[id]/components/QuestionButton.svelte
@@ -1,5 +1,23 @@
+<script lang="ts">
+  export let handleSubmit: () => void;
+  export let handleNext: () => void;
+  export let showCorrectAnswer: boolean;
+</script>
+
 <div class="text-center">
-  <button class="bg-blue-900 text-white font-bold p-4 rounded px-16">
-    Submit
-  </button>
+  {#if showCorrectAnswer}
+    <button
+      class="bg-blue-900 w-[190px] text-white font-bold p-4 rounded px-16"
+      on:click={handleNext}
+    >
+      Next
+    </button>
+  {:else}
+    <button
+      class="bg-blue-900 w-[190px] text-white font-bold p-4 rounded px-16"
+      on:click={handleSubmit}
+    >
+      Submit
+    </button>
+  {/if}
 </div>
