972ff5f64c82c751ae6791fb52f2e3d4397a0b6a
jgbpessoa <jgbpessoa@gmail.com>
2023-07-09 00:40:48 -0300

    feat(quiz): define global store

--- quiz/src/routes/quiz/[id]/+page.svelte
+++ quiz/src/routes/quiz/[id]/+page.svelte
@@ -2,10 +2,17 @@
   import QuestionButton from "./components/QuestionButton.svelte";
   import QuestionOption from "./components/QuestionOption.svelte";
   import QuestionText from "./components/QuestionText.svelte";
+  import { answers, type Answer } from "../../../store";
 
   export let data: any;
 
   let currentQuestionIndex = 0;
+  let answersValue: Answer[];
+
+  answers.subscribe((value) => {
+    answersValue = value;
+  });
+
   $: question = data.questions[currentQuestionIndex];
 </script>
 
